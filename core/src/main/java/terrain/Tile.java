1cff7d9978a7b2a0d3261328e9fc429531904ecd
Joel <joelaxel.olsson@gmail.com>
2026-04-21 11:03:40 +0200

    x och y position för Tile classes

--- core/src/main/java/terrain/Tile.java
+++ core/src/main/java/terrain/Tile.java
@@ -1,7 +1,8 @@
 package terrain;
 
 public class Tile {
-
+    int x;
+    int y;
 
     Terrain terrain;
     //List<Unit> units;
@@ -9,11 +10,19 @@ public class Tile {
     //Building building;
     //Resource resource;
 
-    public Tile() {
-
+    public Tile(int x, int y, Terrain terrain) {
+        this.x = x;
+        this.y = y;
     }
 
     public Terrain getTerrain(){
         return terrain;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
     }
 }
