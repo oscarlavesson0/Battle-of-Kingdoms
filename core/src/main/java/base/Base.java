cb74be17d7f726796d8af77cec96bdd32a433872
EmilHadzic123 <hadzic.emil@yahoo.se>
2026-04-21 10:04:26 +0200

    Update Base.java
    Base attributes

--- core/src/main/java/base/Base.java
+++ core/src/main/java/base/Base.java
@@ -1,4 +1,14 @@
 package base;
 
 public class Base {
+    private int maxHp = 100;
+    private int currentHp = 100;
+
+    public int getMaxHp() {
+        return maxHp;
+    }
+
+    public int getCurrentHp() {
+        return currentHp;
+    }
 }
