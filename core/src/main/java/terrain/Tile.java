7fcc470654c5bb95691dc34ce68087a83360dcf8
Joel <joelaxel.olsson@gmail.com>
2026-04-21 10:42:34 +0200

    Kommentar i Tile

--- core/src/main/java/terrain/Tile.java
+++ core/src/main/java/terrain/Tile.java
@@ -3,9 +3,12 @@ package terrain;
 public class Tile {
     Terrain terrain;
     //List<Unit> units;
+    //Base base;
+    //Building building;
+    //Resource resource;
 
     public Tile() {
-
+        
     }
 
     public Terrain getTerrain(){
