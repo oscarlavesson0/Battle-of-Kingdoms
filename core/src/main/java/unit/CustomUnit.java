419ab0713ff085e8ae8650ee855966a2f086f002
enidbecarevic0 <enidbecarevic0@gmail.com>
2026-05-05 11:23:07 +0200

    Små ändringar
    Har städat unit koden lite

--- core/src/main/java/unit/CustomUnit.java
+++ core/src/main/java/unit/CustomUnit.java
@@ -2,17 +2,14 @@ package unit;
 
 public class CustomUnit extends Unit {
 
-    public CustomUnit(int maxHP, int attack, int speed,
-                      int defence, Weapon weapon, int startX, int startY) {
+    public CustomUnit(int maxHP, int attack, int speed, int defence, Weapon weapon, int startX, int startY) {
         super(maxHP, attack, speed, defence, weapon, startX, startY);
     }
-
     @Override
     public String toString() {
         return String.format(
             "CustomUnit{hp=%d, atk=%d, spd=%d, def=%d, weapon=%s, pos=(%d,%d)}",
-            getMaxHp(), getAttack(), getSpeed(), getDefence(),
-            getWeapon(), getX(), getY()
+            getMaxHp(), getAttack(), getSpeed(), getDefence(), getWeapon(), getX(), getY()
         );
     }
 }
