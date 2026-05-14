ed6386945f69fc4cde202bb65a03121d4bbac11e
EmilHadzic123 <hadzic.emil@yahoo.se>
2026-05-14 13:31:09 +0200

    Update UnitStatsPopup.java

--- core/src/main/java/popup/UnitStatsPopup.java
+++ core/src/main/java/popup/UnitStatsPopup.java
@@ -1,7 +1,6 @@
 package popup;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
