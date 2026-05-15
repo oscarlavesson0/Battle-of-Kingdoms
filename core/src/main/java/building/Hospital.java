0c50cfb4f84dd18242ae020b7add6e2d2b3887f4
Joel <joelaxel.olsson@gmail.com>
2026-05-15 12:35:54 +0200

    Hospital heal metod

--- core/src/main/java/building/Hospital.java
+++ core/src/main/java/building/Hospital.java
@@ -4,12 +4,19 @@ import base.Player;
 
 public class Hospital extends Building{
 
-    public Hospital(int x, int y, Player owner){
-        super(x, y, owner);
+    int healingRange;
+
+    public Hospital(int x, int y, Player owner, int id){
+        super(x, y, owner, id);
+        healingRange = 3;
     }
 
     @Override
     public void ActivateBuildingAbility() {
-
+        for (int i = -healingRange; i < healingRange; i++){
+            for (int j = -healingRange; j < healingRange; j++){
+                // Gör heal på units av samma owner.
+            }
+        }
     }
 }
