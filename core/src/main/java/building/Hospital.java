7da1692435bcece03361136223e16bd38050d4bc
Joel <joelaxel.olsson@gmail.com>
2026-05-15 12:17:13 +0200

    Hospital klass

---
+++ core/src/main/java/building/Hospital.java
@@ -0,0 +1,15 @@
+package building;
+
+import base.Player;
+
+public class Hospital extends Building{
+
+    public Hospital(int x, int y, Player owner){
+        super(x, y, owner);
+    }
+
+    @Override
+    public void ActivateBuildingAbility() {
+
+    }
+}
