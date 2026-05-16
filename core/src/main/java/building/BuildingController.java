ef2ebb73e0251e2d6cac9c6d2908079fe1bf4969
Joel <joelaxel.olsson@gmail.com>
2026-05-16 14:44:40 +0200

    Lista med buildings
    get och setmetoder för building lista

--- core/src/main/java/building/BuildingController.java
+++ core/src/main/java/building/BuildingController.java
@@ -6,12 +6,28 @@ import java.util.List;
 public class BuildingController {
 
     List<BuildingType> buildingTypes;
+    List<Building> buildings;
 
     public BuildingController(){
+        buildings = new ArrayList<>();
         buildingTypes = new ArrayList<>();
         buildingTypes.add(BuildingType.Hospital);
         buildingTypes.add(BuildingType.Barracks);
         buildingTypes.add(BuildingType.Blacksmith);
+    }
+
+    public List<BuildingType> getBuildingTypes() {
+        return buildingTypes;
+    }
+
+    public void addBuilding(Building building){
+        buildings.add(building);
+    }
+
+    public void updateBuildings(){
+        for (Building building : buildings){
+            building.updateBuilding();
+        }
     }
 
 }
