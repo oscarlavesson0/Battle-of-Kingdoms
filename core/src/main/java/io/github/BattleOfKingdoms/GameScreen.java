7264f2e196d24ac79b464c0852a9cedd6eeb8269
Joel <joelaxel.olsson@gmail.com>
2026-06-03 13:13:23 +0200

    Update GameScreen.java

--- core/src/main/java/io/github/BattleOfKingdoms/GameScreen.java
+++ core/src/main/java/io/github/BattleOfKingdoms/GameScreen.java
@@ -109,11 +109,11 @@ public class GameScreen implements Screen {
         grass         = sheet.getTile(0, 5);
         baseGraphic = new Base(sheet, base1);
 
-        base1 = new BaseStats(Player.PLAYER_ONE, tileController.getTileGrid()[2][28]);
-        base2 = new BaseStats(Player.PLAYER_TWO, tileController.getTileGrid()[55][28]);
+        base1 = new BaseStats(Player.PLAYER_ONE, tileController.getTileGrid()[17][28]);
+        base2 = new BaseStats(Player.PLAYER_TWO, tileController.getTileGrid()[40][28]);
 
-        world.placeBaseStructure(baseGraphic, base1, 28,  2);
-        world.placeBaseStructure(baseGraphic, base2, 28, 55);
+        world.placeBaseStructure(baseGraphic, base1, 28,  17);
+        world.placeBaseStructure(baseGraphic, base2, 28, 40);
 
         unitController  = new UnitController(tileController.getTileGrid());
         highlightSystem = new HighlightSystem(unitController);
